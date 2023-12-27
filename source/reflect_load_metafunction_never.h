9bd77f078c12be6f873ec0d142dac61838f30534
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2023-12-26 20:30:29 -0400

    docs(reflect): document the protocol for `load_metafunction`

--- source/reflect_load_metafunction_never.h
+++ source/reflect_load_metafunction_never.h
@@ -4,6 +4,19 @@
 
 namespace cpp2::meta {
 
+//  Protocol of cppfront for program-defined metafunctions
+//
+//  Cppfront lowers a metafunction named 'f' as the `extern "C"` symbol 'cpp2_metafunction_f'
+//
+//  The input to 'load_metafunction' is the id-expression in the meta-functions-list
+//
+//  `load_metafunction' looks up the metafunction in an implementation-defined way
+//
+//  The output of 'load_metafunction'
+//  is a function that forwards to the looked up metafunction, if found,
+//  and an empty function otherwise
+
+//  Returns an empty function
 auto load_metafunction(std::string const&)
     -> std::function<void(type_declaration&)>
 {
